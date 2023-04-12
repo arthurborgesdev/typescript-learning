eda7a50ef837f9a56cef13f5ad395cc488ae1a06
Arthur Borges <arthurborges.dev@gmail.com>
2023-04-12 09:48:23 -0300

    Day 2: Dive into Advanced TypeScript Features
    1 - Study advanced types: union types, intersection types, mapped types, etc.
    2 - Learn about type guards and type inference
    3 - Understand generics and explore their use cases
    4 - Get comfortable with namespaces and modules

---
+++ triangle.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Triangle = void 0;
+var Triangle = /** @class */ (function () {
+    function Triangle() {
+    }
+    return Triangle;
+}());
+exports.Triangle = Triangle;
